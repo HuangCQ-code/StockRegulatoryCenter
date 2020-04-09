cb44f6028025ce2b943e1aef0ed14cd1cbd624c6
weiguodong <2425276494>
2020-04-09 23:18:57 +0800

    sysLog

--- stock-base-dao/src/main/java/com/stock/mapper/SysOperationLogMapper.java
+++ stock-base-dao/src/main/java/com/stock/mapper/SysOperationLogMapper.java
@@ -2,6 +2,8 @@ package com.stock.mapper;
 
 import com.stock.entity.SysOperationLog;
 
+import java.util.List;
+
 public interface SysOperationLogMapper {
     int deleteByPrimaryKey(Long id);
 
@@ -14,4 +16,7 @@ public interface SysOperationLogMapper {
     int updateByPrimaryKeySelective(SysOperationLog record);
 
     int updateByPrimaryKey(SysOperationLog record);
+
+    //新增
+    List<SysOperationLog> findAll();
 }
